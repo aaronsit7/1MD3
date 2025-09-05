336c19ee684ce3f10480140a3cbd4935a364514b
Aaron Sit <161505324+aaronsit7@users.noreply.github.com>
2025-09-05 10:16:43 -0400

    Added exmaple files from lecture

---
+++ Adder.py
@@ -0,0 +1,10 @@
+print("*************** Adder Program 24.11.3. ***************")
+
+#User input values
+x = int(input("Please enter the first number: "))
+y = int(input("Please enter the second number: "))
+
+#Calculate the sum
+print("The sum of these two numbers is:", x + y)
+
+input("Please press enter to terminate.")
