ac8a5ead0be365678120d943d47669ef5ba299f2
aaronsit7 <aaronsit7@gmail.com>
2025-09-10 09:57:14 -0400

    Added first assignment file

---
+++ Assignments/1md3_a0.py
@@ -0,0 +1,10 @@
+print("imd3_a0 Program")
+
+#User input width and length
+width = int(input("Please enter the width of the rectangle: "))
+length = int(input("Please enter the length of the rectangle: "))
+
+#calculate the area and print result
+print("The area of the rectangle is: ", width * length)
+
+input("Please press enter to terminate.")
--- Intro FIles/#Simplify the following statements.py
+++ Intro FIles/#Simplify the following statements.py
@@ -16,3 +16,10 @@ True
 
 a and (not a)
 False
+
+#Add parenthesis to the expression below to be explicit about order
+#of operations
+a and (not b) or (not a) or b
+
+#For what values of "a" and "b" is the following False/True?
+not((a or b) and not (b or not a or not b))
