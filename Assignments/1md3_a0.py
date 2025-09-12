6b3ac51a95f9b799048dd67cecdf9eadacbae50a
aaronsit7 <aaronsit7@gmail.com>
2025-09-12 10:32:23 -0400

    Added new functions from in class examples

--- Assignments/1md3_a0.py
+++ Assignments/1md3_a0.py
@@ -1,8 +1,6 @@
-#Define function to calculate area from width and length
 def area(width, length):
     return width * length
 
-#Print value of area function
 print(area(6, 7))
 
 input("Please press enter to terminate.")
--- Intro FIles/Functions.py
+++ Intro FIles/Functions.py
@@ -6,3 +6,35 @@ print(my_function(9, 1))
 
 a = my_function(4, 4)
 print(a)
+
+tax_rate = 0.17
+
+def calc_tax(income):
+    return tax_rate * income
+
+def get_income(tax):
+    return tax/tax_rate
+
+def get_amount_owed(tax_paid, income):
+    return calc_tax(income) - tax_paid
+
+def f1(x):
+    print(x)
+    print(x)
+    return
+
+def f2(x):
+    print(x)
+    print(x)
+    return x
+
+def f3(x):
+    return x
+    print(x)
+    print(x)
+
+def f4(x):
+    print(x)
+    return x
+    print(x)
+    return x
